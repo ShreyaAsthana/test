524b9765db79cfc6a2ce9effc440d2306880cd37
Shreya <shreya.asthana925@gmail.com>
2021-01-12 23:25:19 +0530

    my first commit

---
+++ src/main/java/escalation_hub_test/escalation_hub_test/App.java
@@ -0,0 +1,13 @@
+package escalation_hub_test.escalation_hub_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
